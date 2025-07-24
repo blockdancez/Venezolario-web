96f824b3761e888a17afa95dfd7bb554fa489ae5
FicsxzOrc <a1272339562@163.com>
2025-07-24 18:38:52 +0800

    feat: 修改静态文件路径 删除不必要的文件

--- src/components/ChapterSelect.tsx
+++ src/components/ChapterSelect.tsx
@@ -73,7 +73,7 @@ export const ChapterSelect: React.FC = () => {
 
           {/* 金币显示 */}
           <div className="flex items-center bg-gradient-to-r from-yellow-400 to-orange-400 rounded-xl px-3 py-2 shadow-lg">
-            <img src="/Sprite/coin.png" alt="coins" className="w-5 h-5 mr-1" />
+            <img src="/static/coin.png" alt="coins" className="w-5 h-5 mr-1" />
             <span className="font-bold text-white text-sm">{progress.coins}</span>
           </div>
         </div>
